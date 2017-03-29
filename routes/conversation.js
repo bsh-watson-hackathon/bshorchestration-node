916748035f69620851ce7c9b092b6eab8153c505
Ralph Retter <ralph.retter@bosch-softtec.com>
2017-03-29 10:34:00 +0200

    modified recipe search

--- routes/conversation.js
+++ routes/conversation.js
@@ -15,15 +15,16 @@
  */
 
 const watson = require('watson-developer-cloud'); // watson sdk
+var request = require('request');
 
 // Create the service wrapper
 const conversation = watson.conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  // username: '<username>',
-  // password: '<password>',
-  version_date: '2016-10-21',
-  version: 'v1'
+    // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+    // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+    // username: '<username>',
+    // password: '<password>',
+    version_date: '2016-10-21',
+    version: 'v1'
 });
 const workspace = process.env.WORKSPACE_ID || '<workspace-id>';
 
@@ -35,102 +36,113 @@ const workspace = process.env.WORKSPACE_ID || '<workspace-id>';
  * @return {Object}          The response with the updated message
  */
 const updateMessage = (input, response, httpresponse) => {
-  console.log("got from conversation: "+JSON.stringify(response,null,2));
-  if (!response.output) {
-    response.output = {};
-  } else {
-
-    if (response.output.action){
-      //got an action
-        if (response.output.action="recipeSearch" && response.context.recipe){
-
-             // call recipe API here
-
-            //return result
-            var message = {
-                workspace_id: workspace,
-                input:{},
-                context:response.context,
-
-            }
-            message.context.foundRecipes = [
-                {"recipe": {
-                    "id":"123",
-                    "name":response.context.recipe
-                }}
-            ]
-            sendMessage(message, httpresponse, updateMessage);
-            return {};
-
-
-
-        }else {
-          response.output.text = "Sorry I did not get that";
-          return response;
-        }
-    }else{
-        return response;
-    }
-
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
+    console.log("got from conversation: " + JSON.stringify(response, null, 2));
+    if (!response.output) {
+        response.output = {};
     } else {
-      responseText = 'I did not understand your intent';
+
+        if (response.output.action) {
+            //got an action
+            if (response.output.action = "recipeSearch" && response.context.recipe) {
+
+                // call recipe API here
+                request('https://bshrecipes.mybluemix.net/recipesmetadata?title=' + response.context.recipe, function (error, reciperesponse, body) {
+                    console.log('error:', error); // Print the error if one occurred
+                    console.log('statusCode:', reciperesponse && reciperesponse.statusCode); // Print the response status code if a response was received
+                    //console.log('body:', body); // Print the HTML for the Google homepage.
+                    //return result
+                    var message = {
+                        workspace_id: workspace,
+                        input: {},
+                        context: response.context,
+
+                    }
+                    var recipes = JSON.parse(body);
+                    message.context.recipeInformation = {
+                        foundNumber: recipes.length,
+                        excludedNumber: 1,
+                        selectedRecipe: {
+
+                            "id": recipes[0].identifier,
+                            "name": recipes[0].data[0].title
+
+                        }
+                    };
+
+                    sendMessage(message, httpresponse, updateMessage);
+                    return {};
+
+
+                });
+
+
+            } else {
+                response.output.text = "Sorry I did not get that";
+                return response;
+            }
+        } else {
+            return response;
+        }
+
     }
-  }
-  response.output.text = responseText;
-  return response;
+    /* if (response.intents && response.intents[0]) {
+     var intent = response.intents[0];
+     // Depending on the confidence of the response the app can return different messages.
+     // The confidence will vary depending on how well the system is trained. The service will always try to assign
+     // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+     // user's intent . In these cases it is usually best to return a disambiguation message
+     // ('I did not understand your intent, please rephrase your question', etc..)
+     if (intent.confidence >= 0.75) {
+     responseText = 'I understood your intent was ' + intent.intent;
+     } else if (intent.confidence >= 0.5) {
+     responseText = 'I think your intent was ' + intent.intent;
+     } else {
+     responseText = 'I did not understand your intent';
+     }
+     }
+     response.output.text = responseText;*/
+    return response;
 };
 
 const sendMessage = (payload, result, callback) => {
-  console.log("sending to conversation: "+ JSON.stringify(payload,null,2));
+    console.log("sending to conversation: " + JSON.stringify(payload, null, 2));
     conversation.message(payload, (error, data) => {
         if (error) {
             console.log("error " + JSON.stringify(error));
             return error.message;
         }
         var answer = updateMessage(payload, data, result);
-        if (answer && answer.output && answer.output.text) {
-            console.log("returning"+answer);
+        if (answer && answer.output && answer.output.text && answer.output.text.length > 0) {
+            console.log("returning" + JSON.stringify(answer));
             return result.json(answer);
         }
     });
 }
 
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.post('/api/message', (req, res, next) => {
-  //  const workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-    if (!workspace || workspace === '<workspace-id>') {
-      return res.json({
-        output: {
-          text: 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' +
-            '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> ' +
-            'documentation on how to set this variable. <br>' +
-            'Once a workspace has been defined the intents may be imported from ' +
-            '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> ' +
-            'in order to get a working application.'
+    app.post('/api/message', (req, res, next) => {
+        //  const workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+        if (!workspace || workspace === '<workspace-id>') {
+            return res.json({
+                output: {
+                    text: 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' +
+                    '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> ' +
+                    'documentation on how to set this variable. <br>' +
+                    'Once a workspace has been defined the intents may be imported from ' +
+                    '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> ' +
+                    'in order to get a working application.'
+                }
+            });
         }
-      });
-    }
-    const payload = {
-      workspace_id: workspace,
-      context: req.body.context || {},
-      input: req.body.input || {}
-    };
+        const payload = {
+            workspace_id: workspace,
+            context: req.body.context || {},
+            input: req.body.input || {}
+        };
 
-    // Send the input to the conversation service
-      sendMessage(payload,res,updateMessage);
-  });
+        // Send the input to the conversation service
+        sendMessage(payload, res, updateMessage);
+    });
 };
